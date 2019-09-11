3943831776445f62647846825379b4e26ea69d5d
hitoTright <915840223@qq.com>
2019-09-11 11:08:06 +0800

    Update helper.php

--- helper.php
+++ helper.php
@@ -2,6 +2,28 @@
 
 use think\Log;
 
+/**
+ * hex颜色加深减淡
+ * @param $color
+ * @param $percent 0-1
+ * @return string
+ */
+function changeColor($color, $percent)
+{
+    if(strpos($color,'#') === false){
+        return '';
+    }
+    $color = str_replace('#', "", $color);
+    $t = $percent < 0 ? 0 : 255;
+    $p = $percent < 0 ? $percent * -1 : $percent;
+    $RGB = str_split($color, 2);
+    $R = hexdec($RGB [0]);
+    $G = hexdec($RGB [1]);
+    $B = hexdec($RGB [2]);
+    return '#' . substr(dechex(0x1000000 + (round(($t - $R) * $p) + $R) * 0x10000 + (round(($t - $G) * $p) + $G) * 0x100
+            + (round(($t - $B) * $p) + $B)), 1);
+}
+
 if (!function_exists('array_column')) {
     function array_column(array $array, $column, $index_key = null)
     {
